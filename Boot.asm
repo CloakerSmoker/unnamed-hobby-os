83543c29f4b9ce8e5bba1af314b6c2f1e386653d
CloakerSmoker <43710474+CloakerSmoker@users.noreply.github.com>
2020-12-25 02:01:54 -0600

    Started over on many things, switched to 1024x768x24 graphical mode

--- Boot.asm
+++ Boot.asm
@@ -11,6 +11,21 @@ sec2 equ 0x02
 	push 0x1400
 	pop es
 	
+	mov ax, 0x4f01
+	mov cx, 0x118 | (1 << 14)
+	mov di, 0x100
+	
+	int 0x10
+	
+	mov ebx, dword [es:di+40]
+FrameBuffer equ 0x7000
+	mov dword [FrameBuffer], ebx
+	
+	mov ax, 0x4f02
+	mov bx, 0x118 | (1 << 14)
+	
+	int 0x10
+	
 	mov bx, 0
 	mov al, 62
 	mov ch, 0
@@ -36,93 +51,27 @@ ret
 
 [BITS 64]
 
-Oct2Bin:
-	xor r8, r8
-	xor r9, r9
-	xor r10, r10
-	mov r11, 11
-	
-	Oct2Bin_Loop:
-		imul r8, 8
-		mov r9b, byte [rcx + r10]
-		
-		sub r9, '0'
-		add r8, r9
-		inc r10
-		dec r11
-		
-		or r11, r11
-		jnz Oct2Bin_Loop
-	ret
-ret
-
-UStar_FindKernel:
-	mov rax, FS_Base
-	xor esi, esi
-	
-	UStar_Loop:
-		add rax, rsi
-		
-		mov ecx, dword [rax + 257]
-		cmp ecx, 'usta'
-		jne UStar_Fail
-		
-		lea rcx, [rax + 0x7c]
-		call Oct2Bin
-		
-		mov ecx, dword [rax]
-		cmp ecx, 'Kern'
-		jne UStar_Next
-		
-		lea rax, [rax + 512]
-		ret
-		
-		UStar_Next:
-			xchg rax, r8
-			xor edx, edx
-			mov ebx, 512
-			idiv ebx
-			imul rax, 512
-			add rax, 512
-			
-			cmp edx, 0
-			jz UStar_Loop_
-			add rax, 512
-			UStar_Loop_:
-			add rax, r8
-	jmp UStar_Loop
-	
-	UStar_Fail:
-	hlt
-
 LongMain:
-	mov rsp, TinyStack
-	call UStar_FindKernel
+	xor ax, ax
+	mov rsp, Stack
+	
+	mov rax, FS_Base
 	
 	mov rbx, [rax + 8]
+	mov r8d, (KernelLimit - KernelStart)
+	
 	xor edi, edi
 LoadKernel:
 	mov rcx, [rax + rdi]
 	mov [rbx + rdi], rcx
-	add rdi, 8
+	inc edi
 	cmp edi, r8d
 	jl LoadKernel
 	
 	mov edx, FS_Base
-	mov rsi, Intrinsics
+	mov esi, [FrameBuffer]
 	mov edi, r8d
 	jmp rbx
-
-SetCR3:
-	mov cr3, rdi
-ret
-GetCR3:
-	mov rax, cr3
-ret
-
-Intrinsics:
-	dq SetCR3
-	dq GetCR3
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
@@ -130,12 +79,16 @@ dw 0xAA55
 TinyStack equ 0x1000
 FS_Base equ 0x14000
 
-INCBIN "Kernel.tar"
+KernelStart equ $
+
+INCBIN "./src/FS/Kernel.bin"
+
+KernelLimit equ $
+
+TIMES 64504 - ($ - $$) db 0
+
+Stack equ $
 
 TIMES 64512 - ($ - $$) db 0
 
 PageTables equ 0x8000
-
-Ext2LoadExtendedBootLoader:
-	mov bx
-	
