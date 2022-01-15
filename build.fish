59e939f17170f71802529d3054c0264d92674b9a
CloakerSmoker <43710474+CloakerSmoker@users.noreply.github.com>
2022-01-15 05:10:21 -0600

    Fixed some ATA-PIO writes blocking forever in QEMU

--- build.fish
+++ build.fish
@@ -1,22 +1,22 @@
-nasm ./src/bootloader/Boot1.asm -o boot1.bin; or exit
+nasm ./src/bootloader/Boot1.asm -o ./build/boot1.bin; or exit
 
 set Options "--elf" "--crlf"
 
-rlx -i ./src/bootloader/Boot2.rlx -o boot2.bin --bin --crlf; or exit
-rlx -i ./src/kernel/Main.rlx -o kernel.elf $Options --debug --dwarf; or exit
-rlx -i ./src/user/TestProgram.rlx -o TestProgram.elf $Options; or exit
-rlx -i ./src/user/Write.rlx -o Write.elf $Options --debug; or exit
-rm disk.img
+rlx -i ./src/bootloader/Boot2.rlx -o ./build/boot2.bin       --bin --crlf; or exit
+rlx -i ./src/kernel/Main.rlx      -o ./build/kernel.elf      $Options --debug --dwarf; or exit
+rlx -i ./src/user/TestProgram.rlx -o ./build/TestProgram.elf $Options; or exit
+rlx -i ./src/user/Write.rlx       -o ./build/Write.elf       $Options --debug --dwarf; or exit
+rm ./build/disk.img
 echo "
 format
 size 2 m
 done
-import-to-boot-sector boot1.bin
-import-to-node boot2.bin 5
+import-to-boot-sector ./build/boot1.bin
+import-to-node ./build/boot2.bin 5
 link-to-node boot2.bin 5
-import kernel.elf kernel.elf
+import ./build/kernel.elf kernel.elf
+import ./build/TestProgram.elf test.elf
+import ./build/Write.elf write
 import TestFile.txt test.txt
-import TestProgram.elf test.elf
-import Write.elf write
 quit
-" | ./Ext2Tool.elf disk.img
+" | ./Ext2Tool.elf ./build/disk.img
