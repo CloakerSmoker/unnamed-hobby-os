ca28a1d7598857a958d762192132e27e5d0d1d5d
CloakerSmoker <43710474+CloakerSmoker@users.noreply.github.com>
2022-01-29 13:42:36 -0600

    Got booting from EFI mostly working

--- efi_build.fish
+++ efi_build.fish
@@ -2,6 +2,12 @@ rm EFIBoot.img
 
 nrlx -i ./src/bootloader/EFIBoot.rlx -o ./build/EFIBoot.efi --pe --crlf --pe-subsystem 10; or exit
 
+set Options "--elf" "--crlf"
+
+nrlx -i ./src/kernel/Main.rlx      -o ./build/kernel.elf      $Options --debug --dwarf; or exit
+nrlx -i ./src/user/TestProgram.rlx -o ./build/TestProgram.elf $Options; or exit
+nrlx -i ./src/user/Write.rlx       -o ./build/Write.elf       $Options --debug --dwarf; or exit
+-i ./src/kernel/Main.rlx -o ./build/kernel.elf --crlf --elf --debug --dwarf
 echo "
 format 120 m
 create
