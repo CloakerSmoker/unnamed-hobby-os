8d218ddabfff97eb06d4db80eb178569b420e715
CloakerSmoker <43710474+CloakerSmoker@users.noreply.github.com>
2022-02-04 15:48:20 -0600

    Updated host tools, added renamed build script

---
+++ build_host_tools.fish
@@ -0,0 +1,3 @@
+nrlx -i ./src/host/GPTTool.rlx -o GPTTool.elf --elf --debug --dwarf; or exit
+nrlx -i ./src/host/FAT32Tool.rlx -o FAT32Tool.elf --elf --debug --dwarf; or exit
+nrlx -i ./src/host/Ext2Tool.rlx -o Ext2Tool.elf --elf --debug --dwarf; or exit
