ed742173ebf1d5fd4be6040f4bb5fb8bd6cf9fd1
CloakerSmoker <43710474+CloakerSmoker@users.noreply.github.com>
2024-01-15 19:26:12 -0600

    Clean up TCP code, properly follow state transitions

--- deps-untracked.fish
+++ deps-untracked.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/env fish
 
-for file in (string split ' ' (cat build/Kernel.d)); git ls-files --error-unmatch $file 2&> /dev/null; if test $status = 1; echo (string replace "$(pwd)" '.' $file)' is not tracked!'; end; end | uniq | grep -v compiler
+for file in (string split ' ' (cat build/*.d)); git ls-files --error-unmatch $file 2&> /dev/null; if test $status = 1; echo (string replace "$(pwd)" '.' $file)' is not tracked!'; end; end | uniq | grep -v compiler
