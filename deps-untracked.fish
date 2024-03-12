39942fcb910931e3a63dae09721abb810277811f
CloakerSmoker <43710474+CloakerSmoker@users.noreply.github.com>
2024-03-12 00:44:13 -0500

    Remove old script

--- deps-untracked.fish
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env fish
-
-for file in (string split ' ' (cat build/Kernel.d)); git ls-files --error-unmatch $file 2&> /dev/null; if test $status = 1; echo (string replace "$(pwd)" '.' $file)' is not tracked!'; end; end | uniq | grep -v compiler
